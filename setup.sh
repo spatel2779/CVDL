33218dcc2081be08955887700793de9779fa1dba
spatel2779 <77871523+spatel2779@users.noreply.github.com>
2024-06-12 18:30:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update && sudo apt-get install -y libgl1-mesa-glx libglib2.0-0
+
 pip install -r requirement.txt
 # Update and install required packages
-sudo apt-get update && sudo apt-get install -y libgl1-mesa-glx libglib2.0-0
